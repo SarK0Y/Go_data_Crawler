a18f6d8b3f91b8e93d0d63beffe774eaf2a4a3f5
SarK0Y <sark0y@protonmail.com>
2026-05-24 02:49:11 +0300

    no build errs, but ... :))

---
+++ src/main0/Anns/tst.java
@@ -0,0 +1,8 @@
+package Anns;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Target ({ElementType.FIELD})
+public @interface tst {
+}

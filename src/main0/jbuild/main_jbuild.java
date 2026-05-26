ed87297ef6a64c839ac18f357c5ae25f58592f3d
SarK0Y <sark0y@protonmail.com>
2026-05-26 16:07:04 +0300

    no build errs, but ... :))

---
+++ src/main0/jbuild/main_jbuild.java
@@ -0,0 +1,33 @@
+package jbuild;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.Modifier;
+import java.util.Optional;
+import jbuild.ifc_jbuild;
+public class main_jbuild implements ifc_jbuild {
+    private String url;
+    private Optional <String> alt_src;
+    private String YourCode;
+    private boolean changed;
+    public main_jbuild (String url, String src, String YourCode) {
+        this.alt_src = Optional.ofNullable (src);
+        this.url = url;
+        this.YourCode = YourCode;
+        this.changed = false;
+    }
+    public static Optional <String> add_code_to_src (String src, String YourCode) {
+        return Optional.empty();
+    }
+    @Override
+    public Optional <String> add_code_to_src () {
+        return Optional.empty();
+    }
+    @Override
+    public void update_src () {
+    }
+}

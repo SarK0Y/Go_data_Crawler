5477232a61864e1e1315e73f55b49074a2ab4b03
SarK0Y <sark0y@protonmail.com>
2026-05-27 02:38:31 +0300

    no build errs, but ... :))

---
+++ src/main0/dbg.n.tst/marker.java
@@ -0,0 +1,12 @@
+package dbg.n.tst;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.reflect.Parameter;
+import java.lang.Class;
+import java.util.ArrayList;
+import java.util.List;
+import Anns.Mark;
+import java.lang.annotation.Annotation;
+public class marker {
+
+}

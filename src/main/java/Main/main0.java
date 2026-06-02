218f8d109cfb7d69b13fa4c02ea2be3970946ace
SarK0Y <sark0y@protonmail.com>
2026-06-02 19:42:34 +0300

    ...

--- src/main/java/Main/main0.java
+++ src/main/java/Main/main0.java
@@ -4,9 +4,9 @@ import static basix_funx._basix_funx.prnt;
 import _lsp._Server;
 import _lsp._TxtDocSrv;
 public class main0{
-    private _Server serv;
-    public void main (String[] args ) {
-        this.serv = new _Server ();
+    private static _Server serv;
+    public static void main (String[] args ) {
+        serv = new _Server ();
         prnt ("tst" + 0);
     }
 }
---
+++ src/main/java/vars/jHome.java
@@ -0,0 +1,3 @@
+public class jHome {
+    static public String v = "";
+}
